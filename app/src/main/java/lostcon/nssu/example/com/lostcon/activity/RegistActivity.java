953346bd0e77465c759a97dca1e19142c7ab878c
minjeong <m950806@naver.com>
2018-08-08 23:50:07 +0900

    Initial commit

---
+++ app/src/main/java/lostcon/nssu/example/com/lostcon/activity/RegistActivity.java
@@ -0,0 +1,16 @@
+package lostcon.nssu.example.com.lostcon.activity;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import lostcon.nssu.example.com.lostcon.R;
+
+public class RegistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regist);
+    }
+}
