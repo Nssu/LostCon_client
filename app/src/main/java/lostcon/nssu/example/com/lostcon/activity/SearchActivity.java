063abb610d5887a2d5d066520fb163686967b2b5
minjeong <m950806@naver.com>
2018-08-11 14:18:26 +0900

    commit image loading

--- app/src/main/java/lostcon/nssu/example/com/lostcon/activity/SearchActivity.java
+++ app/src/main/java/lostcon/nssu/example/com/lostcon/activity/SearchActivity.java
@@ -6,14 +6,22 @@ import android.widget.LinearLayout;
 import com.nhn.android.maps.NMapActivity;
 import com.nhn.android.maps.NMapController;
 import com.nhn.android.maps.NMapView;
+import com.nhn.android.maps.overlay.NMapPOIdata;
+import com.nhn.android.mapviewer.overlay.NMapOverlayManager;
+import com.nhn.android.mapviewer.overlay.NMapPOIdataOverlay;
+import com.nhn.android.mapviewer.overlay.NMapResourceProvider;
 
 import lostcon.nssu.example.com.lostcon.R;
+import lostcon.nssu.example.com.lostcon.map.NMapPOIflagType;
+import lostcon.nssu.example.com.lostcon.map.NMapViewerResourceProvider;
 
 public class SearchActivity extends NMapActivity {
 
     private LinearLayout mapLayout;
     private NMapView mMapView;
     private NMapController mMapController;
+    private NMapResourceProvider mMapViewerResourceProvider;
+    private NMapOverlayManager mOverlayManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +47,34 @@ public class SearchActivity extends NMapActivity {
         mMapController = mMapView.getMapController();
 
 
-
+        mMapView.setEnabled(true);
+        mMapView.setFocusable(true);
+        mMapView.setFocusableInTouchMode(true);
+        mMapView.setScalingFactor(1.5f);
+        mMapView.requestFocus();
         //생성된 네이버 지도 객체를 LinearLayout에 추가시킨다.
         mapLayout.addView(mMapView);
 
+        // create resource provider
+        mMapViewerResourceProvider = new NMapViewerResourceProvider(this);
+        // create overlay manager
+        mOverlayManager = new NMapOverlayManager(this, mMapView, mMapViewerResourceProvider);
+
+        int markerId = NMapPOIflagType.PIN;
+
+        // set POI data
+        NMapPOIdata poiData = new NMapPOIdata(2, mMapViewerResourceProvider);
+        poiData.beginPOIdata(2);
+        poiData.addPOIitem(127.0630205, 37.5091300, "Pizza 777-111", markerId, 0);
+        poiData.addPOIitem(127.061, 37.51, "Pizza 123-456", markerId, 0);
+        poiData.endPOIdata();
+
+        // create POI data overlay
+        NMapPOIdataOverlay poiDataOverlay = mOverlayManager.createPOIdataOverlay(poiData, null);
+
+
+        // show all POI data
+       // poiDataOverlay.showAllPOIdata(0);
     }
 
 
