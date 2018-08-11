1a269c39716c03ec8ad76b08c64d4e3adf398297
minjeong <m950806@naver.com>
2018-08-11 11:46:01 +0900

    commit

--- app/src/main/java/lostcon/nssu/example/com/lostcon/activity/SearchActivity.java
+++ app/src/main/java/lostcon/nssu/example/com/lostcon/activity/SearchActivity.java
@@ -1,17 +1,19 @@
 package lostcon.nssu.example.com.lostcon.activity;
 
 import android.os.Bundle;
-import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.nhn.android.maps.NMapActivity;
+import com.nhn.android.maps.NMapController;
 import com.nhn.android.maps.NMapView;
 
 import lostcon.nssu.example.com.lostcon.R;
 
 public class SearchActivity extends NMapActivity {
 
-    private ViewGroup mapLayout;
+    private LinearLayout mapLayout;
     private NMapView mMapView;
+    private NMapController mMapController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +24,24 @@ public class SearchActivity extends NMapActivity {
     }
 
     public void initMap(){
-        mapLayout = findViewById(R.id.mapLayout);
-
+        //네이버 지도를 넣기 위한 LinearLayout 컴포넌트
+        mapLayout = (LinearLayout) findViewById(R.id.mapLayout);
+        //네이버 지도 객체 생성(xml에 없이)
         mMapView = new NMapView(this);
-        mMapView.setClientId(getResources().getString(R.string.NAVER_API_KEY)); // 클라이언트 아이디 값 설정
+        //네이버 지도 객체에 APIKEY 지정
+        mMapView.setClientId(getResources().getString(R.string.NAVER_API_KEY));
+        //지도를 터치할 수 있도록 옵션 활성화
         mMapView.setClickable(true);
-        mMapView.setEnabled(true);
-        mMapView.setFocusable(true);
-        mMapView.setFocusableInTouchMode(true);
-        mMapView.setScalingFactor(1.5f);
-        mMapView.requestFocus();
+        //확대/축소를 위한 줌 컨트롤러 표시 옵션 활성화
+        mMapView.setBuiltInZoomControls(true, null);
 
+        //지도 객체로부터 컨트롤러 추출
+        mMapController = mMapView.getMapController();
+
+
+
+        //생성된 네이버 지도 객체를 LinearLayout에 추가시킨다.
         mapLayout.addView(mMapView);
-
 
     }
 
